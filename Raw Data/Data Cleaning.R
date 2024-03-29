c492791c0de56c762a4b1ae3ed04fb097b3fa0c7
Ethan Weiland <ethanweiland@Ethans-MacBook-Air-3.local>
2024-03-29 17:49:35 -0400

    Analysis update 2024-03-28

--- Raw Data/Data Cleaning.R
+++ Raw Data/Data Cleaning.R
@@ -3,13 +3,14 @@ library(tidyverse)
 
 # Reading in data
 movies <- read_csv("movies.csv")
-movies <- movies %>% select(id, #selecting variables of interest
+movies <- movies %>% select(id, # selecting variables of interest
                             title,
                             audienceScore,
                             releaseDateTheaters,
                             runtimeMinutes,
-                            originalLanguage,
-                            boxOffice)
+                            genre,
+                            originalLanguage)
+
 movie_reviews <- read_csv("movie_reviews.csv")
 movie_reviews <- movie_reviews %>% select(id, # selecting variables of interest
                                           reviewId,
@@ -21,7 +22,7 @@ TMDB <- TMDB[,-c(1)] # deselecting index column
 # audienceScore
 movies <- movies %>% drop_na(audienceScore) # dropping movies with a missing audienceScore
 
-# critic_score_mean and critic_score_mode
+# critic_score (and review_count)
 movie_reviews <- movie_reviews %>% filter(isTopCritic == TRUE) # only reviews from top critics
  
 movie_reviews$reviewState <- ifelse(movie_reviews$reviewState=="fresh", 1, movie_reviews$reviewState)
@@ -31,10 +32,9 @@ movie_reviews <- movie_reviews %>%
   group_by(id) %>%
   summarise(review_count = sum(!is.na(reviewState)),
             critic_score = sum(reviewState, na.rm=T) / review_count * 100) %>% # % of reviews that are "fresh"
-  filter(review_count >= 2) # at least two valid isTopCritic reviews
-movie_reviews$critic_score_mode <- ifelse(movie_reviews$critic_score >= 50, "Fresh", "Rotten") # if >= 50% of reviews are fresh then "fresh", otherwise "rotten"
+  filter(review_count >= 5) # at least 5 valid isTopCritic reviews
 
-movies <- inner_join(movies, movie_reviews, by="id") # joining to movies table (review_count, critic_score, critic_score_mode)
+movies <- inner_join(movies, movie_reviews, by="id") # joining to movies table
 
 # originalLanguage (need as joining key)
   # Factor w/ 2 levels ("English", "Non-English")
@@ -45,7 +45,7 @@ TMDB$TMDB.original_language <- ifelse(TMDB$TMDB.original_language != "en" & !is.
 TMDB$TMDB.original_language <- ifelse(TMDB$TMDB.original_language == "en", "English", TMDB$TMDB.original_language)
 TMDB$TMDB.original_language <- factor(TMDB$TMDB.original_language)
 
-# Joining in TMDB data (to supplement missing values in releaseDateTheaters and boxOffice)
+# Joining in TMDB data (to supplement missing values in releaseDateTheaters)
 movies <- movies %>%
   left_join(TMDB, by=join_by(title==TMDB.title, # joining on title, run time, and language
                              runtimeMinutes==TMDB.runtime,
@@ -65,50 +65,38 @@ movies <- movies %>% select(-TMDB.release_date)
 sum(is.na(movies$releaseDateTheaters) / length(movies$releaseDateTheaters)) # % missing after supplementing with TMDB data
 movies$releaseDateTheaters <- as.Date(movies$releaseDateTheaters)
 movies <- movies %>% 
-  filter(!is.na(releaseDateTheaters)) # removing movies with missing values on releaseDateTheaters (independent variable)
+  filter(!is.na(releaseDateTheaters)) %>% # removing movies with missing values on releaseDateTheaters
+  mutate(releaseDateTheaters = year(releaseDateTheaters)) # units = years
 
-# boxOffice
-movies$boxOfficegross <- movies$boxOffice # creating boxOfficegross as temporary variable to store cleaned boxOffice values
-movies$boxOfficegross <- gsub("\\$", "",  movies$boxOfficegross) # removing dollar sign
-for (i in 1:length(movies$boxOfficegross)) {
-  if (grepl("M", movies$boxOfficegross[i])) { # for movies that grossed in the millions 
-    movies$boxOfficegross[i] <- gsub("M$", "", movies$boxOfficegross[i])
-    movies$boxOfficegross[i] <- as.numeric(movies$boxOfficegross[i]) * 1000000                              
-  } else if (grepl("K", movies$boxOfficegross[i])) { # for movies that grossed in the thousands
-    movies$boxOfficegross[i] <- gsub("K$", "", movies$boxOfficegross[i])
-    movies$boxOfficegross[i] <- as.numeric(movies$boxOfficegross[i]) *1000
+# genre indicators
+movies$primarygenre <- NA # Only selecting the primary genre
+for (i in 1:nrow(movies)) {
+  if (!is.na(movies$genre[i])) { # for non-missing genres
+    movies$primarygenre[i] <- unlist(regmatches(movies$genre[i], regexec("^[^,]+", movies$genre[i]))) # select everything before the first "," (the primary genre)
   }
 }
-movies$boxOfficegross <- as.numeric(movies$boxOfficegross) # converting to numeric
-
-movies <- movies %>% 
-  mutate(boxOffice = boxOfficegross) %>%
-  select(-boxOfficegross)
-
-sum(is.na(movies$boxOffice)) / length(movies$boxOffice) # % missing before supplementing with TMDB data
-movies$boxOffice <- ifelse(is.na(movies$boxOffice) & !is.na(movies$TMDB.revenue), movies$TMDB.revenue, movies$boxOffice)
-movies <- movies %>% select(-TMDB.revenue)
-sum(is.na(movies$boxOffice)) / length(movies$boxOffice) # % missing after supplementing with TMDB data
-
-movies$boxOffice <- movies$boxOffice / 1000000 # in millions of dollars
-
-# cleaning/standardizing column names
 movies <- movies %>%
-  rename(audience_score = audienceScore,
-         release_date = releaseDateTheaters,
-         runtime = runtimeMinutes,
-         language = originalLanguage,
-         box_office = boxOffice)
+  mutate(genre = primarygenre) %>%
+  select(-primarygenre)
+
+movies$Documentary <- ifelse(movies$genre=='Documentary', 1, 0)
+movies$Horror <- ifelse(movies$genre=='Horror', 1, 0)
+movies$Musical <- ifelse(movies$genre=='Musical', 1, 0)
+movies$War <- ifelse(movies$genre=='War', 1, 0)
+
 movies <- movies %>%
   relocate(id,
            title,
-           audience_score,
+           audienceScore,
            critic_score,
-           critic_score_mode,
-           release_date,
-           box_office,
-           language,
-           runtime,
+           releaseDateTheaters,
+           runtimeMinutes,
+           genre,
+           Documentary,
+           Horror,
+           Musical,
+           War,
+           originalLanguage,
            review_count)
 
 # rotten_tomatoes <- movies
